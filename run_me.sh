18e538341e7d0d6270d3aa73b1390d06920b5c69
MazBin <83834217+Masbin1@users.noreply.github.com>
2022-03-15 10:45:09 +0700

    Add files via upload

---
+++ run_me.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit 2
+fi
+
+printf "%b" "\e[1;34m\nInit Docker Swarm\n\n\e[0m"
+docker swarm init
+sleep 5
+
+printf "%b" "\e[1;34m\n\nCreate Overlay Network\n\n\e[0m"
+docker network create -d overlay --subnet 10.0.1.0/24 --gateway 10.0.1.1 --attachable public
+sleep 5
+
+printf "%b" "\e[1;34m\n\nSet Folder Permission\n\n\e[0m"
+sudo chmod -R 777 "odoo-10/odoo_data"
+sudo chmod -R 777 "odoo-10/postgresql_data"
+sudo chmod -R 777 "odoo-14/odoo_data"
+sudo chmod -R 777 "odoo-14/postgresql_data"
+sleep 5
+
+printf "%b" "\e[1;34m\n\nDeploy PgAdmin4\n\n\e[0m"
+cd "pgadmin"
+docker-compose up -d
+sleep 5
+
+printf "%b" "\e[1;34m\n\nDeploy Odoo 10\n\n\e[0m"
+cd "../odoo-10"
+docker-compose up -d
+sleep 5
+
+printf "%b" "\e[1;34m\n\nDeploy Odoo 14\n\n\e[0m"
+cd "../odoo-14"
+docker-compose up -d
+sleep 5
+
+printf "%b" "\e[1;34m\nDone...\n\n\e[0m"
+printf "%b" "\e[1;34mPgAdmin : http://0.0.0.0:5050\n\e[0m"
+printf "%b" "\e[1;34mEmail PgAdmin : admin@admin.com\n\e[0m"
+printf "%b" "\e[1;34mPassword PgAdmin : admin\n\n\e[0m"
+printf "%b" "\e[1;34mPostgre User : odoo\n\e[0m"
+printf "%b" "\e[1;34mPostgre Password : odoo\n\n\e[0m"
+printf "%b" "\e[1;34mOdoo 10 : http://0.0.0.0:10002\n\e[0m"
+printf "%b" "\e[1;34mMaster Password Odoo 10 : 123\n\n\e[0m"
+printf "%b" "\e[1;34mOdoo 14 : http://0.0.0.0:14002\n\e[0m"
+printf "%b" "\e[1;34mMaster Password Odoo 14 : 123\n\n\e[0m"
